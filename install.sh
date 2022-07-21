d8409d9bb8cf8b6fcd0c838e7f587f1d5e7e51f8
boxedboxedbox <108423875+boxedboxedbox@users.noreply.github.com>
2022-07-21 19:53:36 +0300

    Just realised the project's .gitignore was overriden by my .gitignore, which is installed to ~. (now in git/)

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ read GIT_EMAIL
 printf "Your GitHub account name\n>>> "
 read GITHUB_USERNAME
 
-sed -i "s/\$EMAIL/$GIT_EMAIL/" .gitconfig
-sed -i "s/\$NAME/$GIT_USERNAME/" .gitconfig
-sed -i "s/\$GHNAME/$GITHUB_USERNAME/" .gitconfig
+sed -i "s/\$EMAIL/$GIT_EMAIL/" git/.gitconfig
+sed -i "s/\$NAME/$GIT_USERNAME/" git/.gitconfig
+sed -i "s/\$GHNAME/$GITHUB_USERNAME/" git/.gitconfig
 
 echo Installing configuration files...
 cp -r alacritty ~/.config
@@ -18,8 +18,8 @@ cp -r nvim ~/.config
 cp .bashrc ~/.bashrc
 cp -r bottom ~/.config
 cp -r neofetch ~/.config
-cp .gitconfig ~
-cp .gitignore ~
+cp git/.gitconfig ~
+cp git/.gitignore ~
 
 echo Installing fonts, icons and themes...
 mkdir temp-asdghitryrytiyuuiawd
